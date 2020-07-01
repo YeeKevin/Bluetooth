6e0b6eb36b21b2b638c8ec91f751006fc9464a06
Kevin Yee <Kevinyee92@outlook.com>
2020-07-01 00:57:31 -0400

    Discover other devices

--- app/src/main/java/com/example/bluetooth/MainActivity.kt
+++ app/src/main/java/com/example/bluetooth/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.example.bluetooth
 
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
+
 
 class MainActivity : AppCompatActivity() {
 
     val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    // array list to hold devices
+    var mBTDevices = ArrayList<BluetoothDevice>()
+    var mDeviceListAdapter: DeviceListAdapter? = null
+    // var lvNewDevices: ListView? = null
 
     // Create a BroadcastReceiver for ACTION_FOUND.
     private val receiver = object : BroadcastReceiver() {
@@ -34,9 +42,10 @@ class MainActivity : AppCompatActivity() {
 
                  */
                 BluetoothAdapter.ACTION_STATE_CHANGED -> {
-                    val state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)
+                    val state =
+                        intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR)
 
-                    when(state) {
+                    when (state) {
                         BluetoothAdapter.STATE_OFF -> {
                             Log.d("Testing", "onReceive: STATE OFF")
                         }
@@ -64,19 +73,26 @@ class MainActivity : AppCompatActivity() {
             when(action) {
                 BluetoothAdapter.ACTION_SCAN_MODE_CHANGED -> {
 
-                    val mode = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR)
+                    val mode =
+                        intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR)
 
-                    when(mode) {
+                    when (mode) {
                         // device is in discoverable mode
                         BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE -> {
                             Log.d("Testing", "receiver2: Discoverability enabled")
                         }
                         // device not in discoverable mode
                         BluetoothAdapter.SCAN_MODE_CONNECTABLE -> {
-                            Log.d("Testing", "receiver2: Discoverability disabled, able to receive connections")
+                            Log.d(
+                                "Testing",
+                                "receiver2: Discoverability disabled, able to receive connections"
+                            )
                         }
                         BluetoothAdapter.SCAN_MODE_NONE -> {
-                            Log.d("Testing", "receiver2: Discoverability disabled, not able to receive connections")
+                            Log.d(
+                                "Testing",
+                                "receiver2: Discoverability disabled, not able to receive connections"
+                            )
                         }
                         BluetoothAdapter.STATE_CONNECTING -> {
                             Log.d("Testing", "receiver2: Connecting...")
@@ -85,6 +101,26 @@ class MainActivity : AppCompatActivity() {
                             Log.d("Testing", "receiver2: Connected")
                         }
                     }
+                }
+            }
+        }
+    }
+
+    // for discovering devices
+    private val receiver3 = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String? = intent.action
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    val device: BluetoothDevice =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    mBTDevices?.add(device)
+                    Log.d("Testing", "onReceive: " + device.name + ": " + device.address)
+                    mDeviceListAdapter =
+                        DeviceListAdapter(context, R.layout.device_adapter_view, mBTDevices)
+                    lvNewDevices.adapter = mDeviceListAdapter
+
                 }
             }
         }
@@ -111,6 +147,31 @@ class MainActivity : AppCompatActivity() {
             registerReceiver(receiver2, intentFilter)
         }
 
+        btnFindUnpairedDevices.setOnClickListener {
+            Log.d("Testing", "btnFindUnpairedDevices: Looking for unpaired devices")
+
+            if (bluetoothAdapter != null) {
+                if (bluetoothAdapter.isDiscovering) {
+                    bluetoothAdapter.cancelDiscovery()
+                    Log.d("Testing", "btnFindUnpairedDevices: Cancelling discovery")
+
+                    checkBTPermissions()
+
+                    bluetoothAdapter.startDiscovery()
+                    val discoverDevicesIntent = IntentFilter(BluetoothDevice.ACTION_FOUND)
+                    registerReceiver(receiver3, discoverDevicesIntent)
+                }
+            }
+            if (bluetoothAdapter != null) {
+                Log.d("Testing", "btnFindUnpairedDevices: Now attempting to discover")
+                if(!bluetoothAdapter.isDiscovering) {
+                    checkBTPermissions()
+                    bluetoothAdapter.startDiscovery()
+                    val discoverDevicesIntent = IntentFilter(BluetoothDevice.ACTION_FOUND)
+                    registerReceiver(receiver3, discoverDevicesIntent)
+                }
+            }
+        }
     }
 
     override fun onDestroy() {
@@ -143,6 +204,26 @@ class MainActivity : AppCompatActivity() {
 
             val BTIntent = IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED)
             registerReceiver(receiver, BTIntent)
+        }
+    }
+
+    private fun checkBTPermissions() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            var permissionCheck = checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION")
+            permissionCheck += checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION")
+            if (permissionCheck != 0) {
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ), 1001
+                ) //Any number
+            }
+        } else {
+            Log.d(
+                "Testing",
+                "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP."
+            )
         }
     }
 }
